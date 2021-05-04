c07307c443b2e3669d9ea35439cdb2dc1b304e3b
kafai <lamkafai1997@gmail.com>
2021-05-04 10:27:13 +0800

    regular update [2021-05-04T02:27:13Z]

--- Mackup/.oh-my-zsh-custom/plugins/pdm/pdm.plugin.zsh
+++ Mackup/.oh-my-zsh-custom/plugins/pdm/pdm.plugin.zsh
@@ -1,1 +1,5 @@
-export PYTHONPATH="$HOME/.local/pipx/venvs/pdm/lib/python3.9/site-packages/pdm/pep582":$PYTHONPATH
+if [ -n "$PYTHONPATH" ]; then
+    export PYTHONPATH='/Users/fai/.local/pipx/venvs/pdm/lib/python3.9/site-packages/pdm/pep582':$PYTHONPATH
+else
+    export PYTHONPATH='/Users/fai/.local/pipx/venvs/pdm/lib/python3.9/site-packages/pdm/pep582'
+fi

9942c21e7460c4279e69623966927f13d671ebdd
kafai <lamkafai1997@gmail.com>
2021-06-08 00:10:00 +0800

    regular update [2021-06-07T16:10:00Z]

---
+++ scripts/toggle-fnlock.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title toggle fnlock
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Developer Utils
+
+# Documentation:
+# @raycast.description toggle fnlock
+# @raycast.author Fai
+# @raycast.authorURL https://github.com/kafai97
+
+fnlock=$(defaults read -g com.apple.keyboard.fnState)
+echo $fnlock
+if [[ $fnlock = 1 ]]; then
+  defaults write -g com.apple.keyboard.fnState -bool false
+  echo fnlock is on
+else
+  defaults write -g com.apple.keyboard.fnState -bool true
+  echo fnlock is off
+fi

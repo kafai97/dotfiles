43ec18872d15f332ad39fe9d9a447fed3ef9a3e8
Fai <lamkafai1997@gmail.com>
2021-11-10 08:26:30 +0800

    regular update [2021-11-10T00:26:30+00:00]

--- Mackup/.oh-my-zsh-custom/plugins/dotfiles/dotfiles.plugin.zsh
+++ Mackup/.oh-my-zsh-custom/plugins/dotfiles/dotfiles.plugin.zsh
@@ -4,18 +4,8 @@ dotfiles() {
     case "$1" in
     backup)
         sh $DOTFILES_DIR/scripts/backup.sh
-
-        git -C $DOTFILES_DIR diff-index --quiet HEAD -- || (
-            git -C $DOTFILES_DIR add .
-
-            git -C $DOTFILES_DIR commit -m "regular update [$(gdate -u -Isecond)]"
-
-            git -C $DOTFILES_DIR push
-        )
         ;;
     restore)
-        git -C $DOTFILES_DIR/ pull
-
         sh $DOTFILES_DIR/scripts/restore.sh
         ;;
     *)

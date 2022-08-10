ddc6673f43259ae5ef4796456d2d2d2f7d298d23
Fai <lamkafai1997@gmail.com>
2022-08-10 15:35:04 +0800

    disable tmux

--- dot_config/zsh/plugins/carapace.plugin.zsh
+++ dot_config/zsh/plugins/carapace.plugin.zsh
@@ -1,5 +1,5 @@
 function _carapace_lazy {
     source <(carapace $words[1] zsh)
 }
-compdef _carapace_lazy black carapace docker docker-compose go pip
+compdef _carapace_lazy black carapace curl docker docker-compose go pip
 
